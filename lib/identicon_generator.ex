90e79271832e1a9f3558c41bd061ce4c2d54734f
PedroHamamoto <hamamoto.pedro@gmail.com>
2024-05-12 17:10:14 -0700

    First commit

---
+++ lib/identicon_generator.ex
@@ -0,0 +1,18 @@
+defmodule IdenticonGenerator do
+  @moduledoc """
+  Documentation for `IdenticonGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IdenticonGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
