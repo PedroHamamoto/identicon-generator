c25c1a26afc5b51bd56cb3e076c54459b42c6bc9
PedroHamamoto <hamamoto.pedro@gmail.com>
2024-05-12 17:40:44 -0700

    Implemeting the username hash function

--- lib/identicon_generator.ex
+++ lib/identicon_generator.ex
@@ -1,18 +1,13 @@
 defmodule IdenticonGenerator do
-  @moduledoc """
-  Documentation for `IdenticonGenerator`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> IdenticonGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main(username) do
+    username
+    |> hash_username
   end
+
+  defp hash_username(username) do
+    :crypto.hash(:md5, username)
+    |> :binary.bin_to_list
+  end
+
 end
